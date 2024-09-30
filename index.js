00776a756c61c3a69b08d7d13d36ba0c55c2f6f2
Conner <69326416+ConConner@users.noreply.github.com>
2024-10-01 00:34:40 +0200

    Inintial Push of CCCC
    ConCons Crazy Content with 3 initial hacks pushed

---
+++ index.js
@@ -0,0 +1,46 @@
+function getDbContent() {
+    return fetch('/data/database/content.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('File could not be loaded');
+            }
+            return response.json();
+        })
+}
+
+function addDataToSite(data) {
+    console.log(data)
+
+    const contentContainer = document.getElementById('content-container');
+    data.content.forEach(hack => {
+
+        //Create card for each hack
+        const hackCard = document.createElement('aside');
+        hackCard.setAttribute('linked_card', '');
+        hackCard.setAttribute('image_card', '');
+        hackCard.addEventListener('click', function() {
+            window.location.href = `/viewcontent?id=${encodeURIComponent(hack.id)}`
+        })
+
+        //Get Image
+        const hackImage = document.createElement('img')
+        hackImage.src = hack.images[0];
+        hackImage.alt = hack.name;
+
+        //Create Title
+        const hackName = document.createElement('h1');
+        hackName.textContent = hack.name;
+
+        //Append elements
+        hackCard.appendChild(hackImage);
+        hackCard.appendChild(hackName);
+        contentContainer.appendChild(hackCard);
+
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Get Content
+    var data = getDbContent().then(data => addDataToSite(data));
+})
---
+++ viewcontent/index.js
@@ -0,0 +1,77 @@
+const userLocale = navigator.language || 'en-US';
+
+function getDbContent() {
+    return fetch('/data/database/content.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('File could not be loaded');
+            }
+            return response.json();
+        })
+}
+
+function getDownloadLink(patch, name=null) {
+    href = document.createElement('a')
+    href.href = patch.path;
+    if (!name) href.textContent = patch.name;
+    else href.textContent = name;
+
+    return href;
+}
+
+function addDataToSite(data, params) {
+    const item = data.content.find(i => i.id === params);
+    if (!item) return;
+
+    const title = document.getElementById('title');
+    const desc = document.getElementById('description');
+    const images = document.getElementById('images');
+    const downloads = document.getElementById('downloads');
+    const quickdownload = document.getElementById('quick-download');
+    const authors = document.getElementById('authors');
+
+    document.getElementById('download-heading').textContent = "Versions"
+
+    title.textContent = item.name;
+    desc.textContent = item.description;
+
+    //Authors
+    authors.textContent = item.authors.join(' - ');
+
+    //Images
+    item.images.forEach(image => {
+
+        imageElement = document.createElement('img');
+        imageElement.src = image;
+        images.appendChild(imageElement);
+
+    });
+
+    //Downloads
+    quickdownload.appendChild(getDownloadLink(item.patches[0], "Download"));
+    item.patches.forEach(patch => {
+
+        listItem = document.createElement('li')
+
+        //Get date created
+        var date = new Date(patch.date);
+        var formattedDate = date.toLocaleDateString(userLocale, {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+        listItem.textContent = `${formattedDate}: `
+
+        listItem.appendChild(getDownloadLink(patch));
+        downloads.appendChild(listItem);
+
+    })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Get Content
+    var params = new URLSearchParams(window.location.search);
+    var contentParam = params.get('id')
+    var data = getDbContent().then(data => addDataToSite(data, contentParam));
+})
